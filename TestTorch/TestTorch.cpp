ece0230770340e4a0fd4e5c4e58a2aa539a8d85c
Arun Joshi <jarun6069@gmail.com>
2025-03-26 17:13:10 +0545

    C++ with Visual Studio

---
+++ TestTorch/TestTorch.cpp
@@ -0,0 +1,12 @@
+﻿// TestTorch.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <torch/torch.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
